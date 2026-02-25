37523a54852c526c297d929d7f0b30a99d68076e
Guilherme Ioshua Sene <161888404+guiioshua@users.noreply.github.com>
2026-02-24 22:05:42 -0300

    ex3_1

---
+++ aula_oop_ex3_1.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        double gastosJaneiro = 15000.00;
+        double gastosFevereiro = 23000.00;
+        double gastosMarco = 17000.00;
+        double gastosTotais = gastosJaneiro + gastosJaneiro + gastosMarco;
+
+        System.out.printf("Despesas totais: %.2f", gastosTotais);
+    }
+}
