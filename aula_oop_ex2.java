03cacd57434757180ed895ad372d849742d48876
Guilherme Ioshua Sene <161888404+guiioshua@users.noreply.github.com>
2026-02-24 21:56:19 -0300

    ex2 correção final

--- aula_oop_ex2.java
+++ aula_oop_ex2.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Nova mensagem");
-        Syste.out.println("Mais uma nova mensagem");
+        System.out.println("Mais uma nova mensagem");
     }
 }
