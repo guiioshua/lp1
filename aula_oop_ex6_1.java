c840481fae76ef9f156e1edc7536a426fb377a3c
Guilherme Ioshua Sene <161888404+guiioshua@users.noreply.github.com>
2026-02-25 21:56:38 -0300

    ex6_1

---
+++ aula_oop_ex6_1.java
@@ -0,0 +1,48 @@
+public class LojaMegaVale {
+    
+    static class Shampoo {
+        String marca;
+        String tipo;
+        String tipoEmbalagem;
+        double volume;
+        
+        public Shampoo(String marca, String tipo, String tipoEmbalagem, double volume) {
+            this.marca = marca;
+            this.tipo = tipo;
+            this.tipoEmbalagem = tipoEmbalagem;
+            this.volume = volume; // Correção: nome da variável
+        }
+        
+        public void abrir() {
+            System.out.println("Abriu");
+        }
+        
+        public void apertar(double qtdApertada) {
+            this.volume -= qtdApertada;
+        }
+    }
+
+    static class Secador {
+        String marca;
+        String cor;
+        double potenciaEmWatts;
+        
+        public Secador(String marca, String cor, double potenciaEmWatts) {
+            this.marca = marca;
+            this.cor = cor;
+            this.potenciaEmWatts = potenciaEmWatts;
+        } 
+       
+        public void ligar() {
+            System.out.println("VUUUUUUUUUUUUM");
+        } 
+    }
+    
+    public static void main(String[] args) {
+        Shampoo shampoo = new Shampoo("TRESemmé", "Antiresíduo", "Válvula Pump", 1000.0);
+        shampoo.abrir();
+        
+        Secador secador = new Secador("Tourmaline", "preto", 2100.0);
+        secador.ligar();
+    }
+}
